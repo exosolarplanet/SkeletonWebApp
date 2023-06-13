c2dcd80711328f956d09ab8e65df31e10fc63239
exosolarplanet <ecedenniz@gmail.com>
2023-06-13 11:08:11 +0100

    update

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -12,7 +12,7 @@ public class QueryProcessor {
           + "writer in the English language and the world's pre-eminent dramatist.";
     }
 
-    if (query.toLowerCase().contains("What is your name?")) {
+    if (query.toLowerCase().contains("your name?")) {
       return "Ece";
     }
 
